f60d46a4651b448bb88fff59ae535fc82eb8926b
RuAleksandr <rualeksandr07@gmail.com>
2024-02-11 22:10:58 +0500

    Код рабил на отдельные функции

--- index.js
+++ index.js
@@ -2,95 +2,54 @@ import puppeteer from 'puppeteer';
 import cron from 'node-cron';
 import fs from 'fs';
 
-(async () => {
+const URL_AVITO_PROFILE = 'https://www.avito.ru/user/f9c2c9b95c387c86227f847540f011db/profile';
+const PARSIN_FREQUENCY = '*/2 * * * *'; // Сейчас 1 раз в 2 минуты (на этапе тестирования, после надо сократить до 1 раз в час)
+let dataFromDatabase;
+let dataForRecording = [];
 
-    let dataOld = {};
-
+parserAvitoProfile();
+async function parserAvitoProfile() {
     const browser = await puppeteer.launch({
         userDataDir: '/tmp/myChromeSession',
         headless: false
     });
-
     const page = await browser.newPage();
-
     await page.setViewport({
         width: 1440,
         height: 900
     });
-
-    await page.goto('https://www.avito.ru/user/f9c2c9b95c387c86227f847540f011db/profile')
+    await page.goto(URL_AVITO_PROFILE)
     .catch((err) => console.log("Не получилось стартовать, т.к. не открылась страница профиля «Techno Trade» на Авито!"));
-
     await page.waitForTimeout(1000);
-
-    await cron.schedule('*/2 * * * *', function() {
+    await cron.schedule(PARSIN_FREQUENCY, function() {
         (async () => { 
             try {
+                await getDataFromDatabase();
                 await page.evaluate(() => {
                     location.reload(true)
+                    console.log('2');
                 })
                 await page.waitForTimeout(2000);
                 await autoScroll(page, 50000);
-
                 let allAds = await page.$$('[data-marker="profile-items"] > div > div', e => e);
-
-                let dataInIteration = {
-                    data: {},
-                    ids: [],
-                    count: 0
-                };
-
                 for(let elem of allAds) {
                     let name = await elem.$eval('a', e => e.getAttribute('title'));
+                    name = name.split('«')[1].split('»')[0]
                     let url = await elem.$eval('a', e => e.getAttribute('href'));
                     let id = url.split('_').pop();
                     let price = await elem.$eval('[itemprop="price"]', e => e.getAttribute('content'));
                     let img = await elem.$eval('img', e => e.getAttribute('src'));
-
-                    dataInIteration.ids.push(id);
-
-                    let count = dataInIteration.count;
-                    dataInIteration.data[count] = {};
-
-                    dataInIteration.data[count]['id'] = id;
-                    dataInIteration.data[count]['title'] = name;
-                    dataInIteration.data[count]['url'] = url;
-                    dataInIteration.data[count]['price'] = price;
-                    dataInIteration.data[count]['img_small'] = img;
-
-                    dataInIteration.count++;
+                    handlerSingleAd(name, url, id, price, img)
                 }
-
-                console.log(dataInIteration);
-
-                let profileItemsActive = Number(await page.$eval('[data-marker="profile-tab(active)"]', e => e.innerText.split(' ')[0]));
-                
-                console.log(`Активных объявлений на авито: ${profileItemsActive}. Получилось спарсить: ${dataInIteration.count}. Не удалось спарсить: ${profileItemsActive - dataInIteration.count}`);
-
-
-                if (dataOld.ids) {
-                    console.log('Это повторная итерация и мне есть с чем сравнить. Сейчас узнаю что изменилось.')
-                } else {
-                    console.log('Это первая итерация и мне не с чем сравнивать. Все объявления для меня новые!')
-                    dataOld = dataInIteration;
-                };
-
-
-                // try {
-                //     fs.writeFileSync('/Users/rualeksandr/Development/web-scraper-nodejs/data.json', JSON.stringify(dataInIteration), 'utf8');
-                // } catch (err) {
-                //     console.error(err)
-                // }
-
             } catch (err){
-                console.log('Пропущена итерация обновления страницы, т.к. на странице сейчас отсутствуют нужные данные!');
+                console.log(`Пропущена итерация обновления страницы, т.к. на странице сейчас отсутствуют нужные данные! ${err}`);
             }
-
         })();
+        console.log(dataForRecording);
+        console.log(dataForRecording.length);
     });
-})();
+}
 
-// Автоскрол
 async function autoScroll(page, maxScrolls){
     await page.evaluate(async (maxScrolls) => {
         await new Promise((resolve) => {
@@ -112,15 +71,29 @@ async function autoScroll(page, maxScrolls){
     }, maxScrolls);
 }
 
-
-async function addAds(ids){
-
+// getDataFromDatabase();
+function getDataFromDatabase() {
+    const data = fs.readFileSync('data.json', { encoding: 'utf8', flag: 'r' });
+    dataFromDatabase = JSON.parse(data)['data'];
 }
 
-function updateAds(ids){
-
-}
-
-function deleteAds(ids){
-
+function handlerSingleAd(name, url, id, price, img) {
+    let objForPush = {};
+    for (let i = 0; i < dataFromDatabase.length; i++) {
+        if(id == dataFromDatabase[i]['id']){
+            console.log(`id: ${id} уже есть в бд!`);
+            objForPush = dataFromDatabase[i];
+            dataForRecording.push(objForPush);
+            objForPush = {};
+            return;
+        }
+    }
+    console.log(`id: ${id} нет в бд!`);
+    objForPush["id"] = id;
+    objForPush["title"] = name;
+    objForPush["url"] = url;
+    objForPush["price"] = price;
+    objForPush["img_small"] = img;
+    dataForRecording.push(objForPush);
+    objForPush = {};
 }
