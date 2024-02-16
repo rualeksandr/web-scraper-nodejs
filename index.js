d00034a1b87c892f4c38b7c3049c11c44669cc2e
RuAleksandr <rualeksandr07@gmail.com>
2024-02-16 10:56:58 +0500

    Микросервисный вариант

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+'use strict'; 
 import puppeteer from 'puppeteer';
 import cron from 'node-cron';
 import fs from 'fs';
 
 const URL_AVITO_PROFILE = 'https://www.avito.ru/user/f9c2c9b95c387c86227f847540f011db/profile';
-const PARSIN_FREQUENCY = '*/2 * * * *'; // Сейчас 1 раз в 2 минуты (на этапе тестирования, после надо сократить до 1 раз в час)
-let dataFromDatabase;
+const PARSING_FREQUENCY_PROFILE = '*/2 * * * *';
+const PARSING_FREQUENCY_PRODUCT = '*/2 * * * *';
+let dataFromDatabase = [];
 let dataForRecording = [];
 
-parserAvitoProfile();
+startParser();
+async function startParser() {
+    await getDataFromDatabase()
+    await parserAvitoProfile();
+    parserAvitoProduct()
+}
+
 async function parserAvitoProfile() {
     const browser = await puppeteer.launch({
-        userDataDir: '/tmp/myChromeSession',
+        userDataDir: '/tmp/chromeSessionForParserAvitoProfile',
         headless: false
     });
     const page = await browser.newPage();
@@ -21,7 +29,7 @@ async function parserAvitoProfile() {
     await page.goto(URL_AVITO_PROFILE)
     .catch((err) => console.log("Не получилось стартовать, т.к. не открылась страница профиля «Techno Trade» на Авито!"));
     await page.waitForTimeout(1000);
-    await cron.schedule(PARSIN_FREQUENCY, function() {
+    cron.schedule(PARSING_FREQUENCY_PROFILE, function() {
         (async () => { 
             try {
                 await getDataFromDatabase();
@@ -44,9 +52,8 @@ async function parserAvitoProfile() {
             } catch (err){
                 console.log(`Пропущена итерация обновления страницы, т.к. на странице сейчас отсутствуют нужные данные! ${err}`);
             }
+            await updateDatainDatabase();
         })();
-        console.log(dataForRecording);
-        console.log(dataForRecording.length);
     });
 }
 
@@ -72,28 +79,82 @@ async function autoScroll(page, maxScrolls){
 }
 
 // getDataFromDatabase();
-function getDataFromDatabase() {
-    const data = fs.readFileSync('data.json', { encoding: 'utf8', flag: 'r' });
-    dataFromDatabase = JSON.parse(data)['data'];
+async function getDataFromDatabase() {
+    try {
+        const data = fs.readFileSync('data.json', { encoding: 'utf8', flag: 'r' });
+        dataFromDatabase = JSON.parse(data)['data'];
+      } catch (err) {
+        dataFromDatabase = [];
+        console.log(`Файла нет (похоже это первый запуск): ${err}`);
+    }
 }
 
 function handlerSingleAd(name, url, id, price, img) {
     let objForPush = {};
     for (let i = 0; i < dataFromDatabase.length; i++) {
         if(id == dataFromDatabase[i]['id']){
-            console.log(`id: ${id} уже есть в бд!`);
             objForPush = dataFromDatabase[i];
             dataForRecording.push(objForPush);
             objForPush = {};
             return;
         }
     }
-    console.log(`id: ${id} нет в бд!`);
     objForPush["id"] = id;
     objForPush["title"] = name;
     objForPush["url"] = url;
     objForPush["price"] = price;
     objForPush["img_small"] = img;
+    objForPush["parse"] = {};
+    objForPush["parse"]["status"] = "low";
+    objForPush["vkPost"] = {};
+    objForPush["vkPost"]["status"] = "unpublished";
+    objForPush["vkProduct"] = {};
+    objForPush["vkProduct"]["status"] = "unpublished";
+    objForPush["tgPost"] = {};
+    objForPush["tgPost"]["status"] = "unpublished";
     dataForRecording.push(objForPush);
     objForPush = {};
+}
+
+async function updateDatainDatabase() {
+    if(dataForRecording.length) {
+        let formatedData = {};
+        formatedData["data"] = dataForRecording;
+        let data = JSON.stringify(formatedData, null, 2); 
+        fs.writeFileSync('data.json', data); 
+        console.log("Записан файл data.json!");
+    } else {
+        console.log("При попытке записать в БД обновленные данные, они пришли пустые!");
+    }
+    dataForRecording = [];
+    return;
+}
+
+async function parserAvitoProduct() {
+    const browser = await puppeteer.launch({
+        userDataDir: '/tmp/chromeSessionForParserAvitoProduct',
+        headless: false
+    });
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 1440,
+        height: 900
+    });
+    cron.schedule(PARSING_FREQUENCY_PRODUCT, function() {
+        if (dataFromDatabase.length) {
+
+
+
+
+
+
+        } else {
+            console.log(`В переменно dataFromDatabase поканичего нет, пропускаю апдейт данных!`)
+        }
+
+
+
+        // (async () => { 
+        // })();
+    });
 }
