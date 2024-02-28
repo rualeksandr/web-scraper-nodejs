44cf17dfc836519962a72316cc2c914fdf0e4989
RuAleksandr <rualeksandr07@gmail.com>
2024-02-28 16:37:36 +0500

    vk_add_market.js добавляет изображение для товара

--- vk_add_market.js
+++ vk_add_market.js
@@ -1,58 +1,63 @@
 'use strict'; 
 import fs from 'fs';
 import axios from 'axios';
+import FormData from 'form-data';
 import { VK_API } from './token_private.js';
 
-await market_upload_img('./images/3237220546/0.webp');
+async function uploadProductPhoto(imgPath) {
+    try {
+        const uploadUrl = await getPhotoUploadServer();
+        const uploadResponse = await uploadPhoto(uploadUrl, imgPath);
+        const savedPhoto = await saveProductPhoto(uploadResponse);
 
-async function market_upload_img(img_path) {
-    const upload_url = await getProductPhotoUploadServer();
-    const upload_response = await photosUpload(upload_url, img_path);
-    console.log(JSON.stringify(upload_response));
-    const obj_photo_id = await photosSave(upload_response);
-    console.log(JSON.stringify(obj_photo_id));
+        console.log("Фотография успешно загружена и сохранена:", savedPhoto);
+    } catch (error) {
+        console.error("Ошибка при загрузке фотографии товара: ", error);
+    }
 }
 
-// Метод market.getProductPhotoUploadServer возвращает адрес для загрузки изображений товаров в сообщество.
-async function getProductPhotoUploadServer() {
-    const {data} = await axios.post('https://api.vk.com/method/market.getProductPhotoUploadServer', {
-            access_token: VK_API.access_token,
-            v: VK_API.v,
-            group_id: VK_API.group_id
-        }, {
-            headers: {
-            'Content-Type': 'multipart/form-data'
-            }
-        }
-    )
-    return JSON.stringify(data.response.upload_url);
-}
-
-// Передача файла на сервер. В ответе вернётся объект, содержащий служебную информацию о загруженном изображении.
-async function photosUpload(upload_url, img_path) {
-    const {data} = await axios.post(upload_url, {
-            file: fs.createReadStream(img_path),
-        }, {
-            headers: {
-            'Content-Type': 'multipart/form-data'
-            }
-        }
-    )
-    return data;
-}
-
-// Метод market.saveProductPhoto сохраняет фотографии в альбом после их успешной загрузки на сервер.
-// Возвращает объект с полем photo_id, которое содержит идентификатор загруженного изображения.
-async function photosSave(upload_response) {
-    const {data} = await axios.post('https://api.vk.com/method/market.saveProductPhoto', {
+async function getPhotoUploadServer() {
+    const { data } = await axios.post('https://api.vk.com/method/market.getProductPhotoUploadServer', {
         access_token: VK_API.access_token,
-        v: VK_API.v,    
-        upload_response: upload_response,
+        v: VK_API.v,
+        group_id: VK_API.group_id
     }, {
         headers: {
         'Content-Type': 'multipart/form-data'
         }
-    }
-    )
-    return data;
+    });
+    return data.response.upload_url;
 }
+
+async function uploadPhoto(url, imgPath) {
+    const form = new FormData();
+    form.append('file', fs.createReadStream(imgPath));
+    const request_config = {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            ...form.getHeaders()
+        }
+    };
+    const { data } = await axios.post(url, form, request_config);
+    return JSON.stringify(data);
+}
+
+async function saveProductPhoto(uploadResponse) {
+    const form = {
+        'access_token': VK_API.access_token,
+        'v': VK_API.v,
+        'upload_response': uploadResponse
+    }
+    const { data } = await axios.post('https://api.vk.com/method/market.saveProductPhoto', form, {
+        headers: {
+        'Content-Type': 'multipart/form-data'
+        }
+    });
+    if (data.response.photo_id) {
+        return data.response.photo_id;
+    } else {
+        throw new Error("Ошибка при сохранении фотографии товара. Ответ: ", data);
+    }
+}
+
+uploadProductPhoto('./images/3237220546/0.webp');
